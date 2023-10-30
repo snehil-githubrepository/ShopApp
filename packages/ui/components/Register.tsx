5d7171a4a49c3c96ac1e5802c13510f3b7dff466
Snehil's Github <69778988+snehil-githubrepository@users.noreply.github.com>
2023-10-30 21:39:31 +0530

    added tailwind to my packages folder

--- packages/ui/components/Register.tsx
+++ packages/ui/components/Register.tsx
@@ -1,4 +1,78 @@
+import React from "react";
+
 function Register() {
-  return <div>Register Component</div>;
+  return (
+    <div>
+      <form className="space-y-6" action="#">
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+          Create an Account
+        </h5>
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            placeholder="name@company.com"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="confirmPassword"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            placeholder="••••••••"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Create Account
+        </button>
+        <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+          Already registered?{" "}
+          <a
+            href="/login"
+            className="text-blue-700 hover:underline dark:text-blue-500"
+          >
+            Log in
+          </a>
+        </div>
+      </form>
+    </div>
+  );
 }
+
 export default Register;
